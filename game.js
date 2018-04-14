e6110f8e83fc6ca9b4955ecfd3f9522d25fcdc1e
n2shield <chrismorrissey1985@gmail.com>
2018-04-14 09:28:08 -0400

    Add files via upload

---
+++ game.js
@@ -0,0 +1,27 @@
+// Create a list of words
+var listOfWords = [
+    'bootcamp',
+    'code',
+    'javascript',
+    'mysql',
+    'request',
+    'export',
+    'backend',
+    'frontend',
+    'developer',
+    'vscode',
+    'case',
+    'western',
+    'california',
+    'sunshine',
+    'github',
+    'google',
+    'guniea',
+    'pig',
+    'antidisestablishmentarianism',
+    'stars',
+    'mustache'
+  ];
+  
+  // Export the list of words to other hangman script(s)
+  module.exports = listOfWords;
